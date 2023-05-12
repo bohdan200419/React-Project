88985aba244145c00ce9e6aa8a6e3266ceec605b
Datek1 <yurarudkevich81@gmail.com>
2023-05-12 23:23:22 +0300

    Base

---
+++ src/constants/urls.ts
@@ -0,0 +1,13 @@
+import {axiosService} from "../services/axios.service";
+
+const baseURL = 'https://api.themoviedb.org/3/discover/movie'
+
+const urls = {
+
+}
+
+
+export {
+    baseURL,
+    urls
+}
